93316b3c613147922fbe884e4c9960743827af04
rgblab <my.rgb.lab@gmail.com>
2019-10-29 06:24:04 +0100

    removed skin dropdown from video and audio new pot file

--- components/video/templates/meta-box.php
+++ components/video/templates/meta-box.php
@@ -6,8 +6,6 @@ $skin_key              = 1;
 $skin_value            = $meta_value[ $skin_key ];
 $skin_input_field_attr = $input_field_attr . '[' . $skin_key . ']';
 
-// FIXME remove skin
-
 ?>
 
 <div class="upf-control upf-control--audio">
@@ -15,14 +13,7 @@ $skin_input_field_attr = $input_field_attr . '[' . $skin_key . ']';
         <label class="upf-control__label" for="<?php echo esc_attr( $input_field_attr . '[0]' ); ?>"><?php esc_html_e( 'Enter URL pointing to video file or video streaming provider', 'upf' ) ?></label>
         <input class="upf-control__text-field" type="text" name="<?php echo esc_attr( $input_field_attr . '[0]' ); ?>" value="<?php echo esc_attr( $meta_value[0] ); ?>">
     </div>
-    <div class="upf-control__select">
-        <label class="upf-control__label" for="<?php echo esc_attr( $skin_input_field_attr ); ?>"><?php esc_html_e( 'Set content skin', 'upf' ) ?></label>
-        <select class="upf-control__select-field" name="<?php echo esc_attr( $skin_input_field_attr ); ?>">
-            <option value="global" <?php echo ( 'global' === $skin_value ) ? 'selected' : ''; ?>><?php esc_html_e( 'Global from the customizer', 'upf' ); ?></option>
-            <option value="light" <?php echo ( 'light' === $skin_value ) ? 'selected' : ''; ?>><?php esc_html_e( 'Light', 'upf' ); ?></option>
-            <option value="dark" <?php echo ( 'dark' === $skin_value ) ? 'selected' : ''; ?>><?php esc_html_e( 'Dark', 'upf' ); ?></option>
-        </select>
-    </div>
+    <input type="hidden" name="<?php echo esc_attr( $skin_input_field_attr ); ?>">
 </div>
 <p>
 	<?php echo sprintf( '%1s <a href="https://wordpress.org/support/article/embeds/#okay-so-what-sites-can-i-embed-from" target="_blank">%2s %3s</a>', esc_html__( 'You can use all of these', 'upf' ), esc_html__( 'web sites', 'upf' ), uberPostFormatsHelper::getSVG( 'external-link' ) ); ?>
