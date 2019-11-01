835b88d5ac62345586b8e2bbd2059ce0338c62d7
rgblab <my.rgb.lab@gmail.com>
2019-11-01 04:55:23 +0100

    label case tweak, backend control tweak

--- components/link/templates/meta-box.php
+++ components/link/templates/meta-box.php
@@ -16,11 +16,11 @@ $skin_input_field_attr = $input_field_attr . '[' . $skin_key . ']';
     <div class="upf-control__select">
         <label class="upf-control__label" for="<?php echo esc_attr( $input_field_attr . '[1]' ); ?>"><?php esc_html_e( 'Set where to open link', 'upf' ) ?></label>
         <select class="upf-control__select-field" name="<?php echo esc_attr( $input_field_attr . '[1]' ); ?>">
-            <option value="_blank" <?php echo ( '_blank' === $meta_value[1] ) ? 'selected' : ''; ?>><?php esc_html_e( 'New Browser Tab', 'upf' ); ?></option>
-            <option value="_self" <?php echo ( '_self' === $meta_value[1] ) ? 'selected' : ''; ?>><?php esc_html_e( 'Same Browser Tab', 'upf' ); ?></option>
+            <option value="_blank" <?php echo ( '_blank' === $meta_value[1] ) ? 'selected' : ''; ?>><?php esc_html_e( 'New browser tab', 'upf' ); ?></option>
+            <option value="_self" <?php echo ( '_self' === $meta_value[1] ) ? 'selected' : ''; ?>><?php esc_html_e( 'Same browser tab', 'upf' ); ?></option>
         </select>
     </div>
-    <div class="upf-control__select">
+    <div class="upf-control__select upf-control--no-bottom-margin">
         <label class="upf-control__label" for="<?php echo esc_attr( $skin_input_field_attr ); ?>"><?php esc_html_e( 'Set content skin', 'upf' ) ?></label>
         <select class="upf-control__select-field" name="<?php echo esc_attr( $skin_input_field_attr ); ?>">
             <option value="global" <?php echo ( 'global' === $skin_value ) ? 'selected' : ''; ?>><?php esc_html_e( 'Global from the customizer', 'upf' ); ?></option>
